92b705e7f192adfeb431ea110ee347b4070018b5
Simone Fittipaldi <fittipaldi.s@hotmail.it>
2023-10-19 17:28:26 +0200

    https://learn.microsoft.com/en-us/training/modules/secure-aspnet-core-identity/3-add-identity

---
+++ ContosoPizza/Pages/AdminsOnly.cshtml.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoPizza.Pages
+{
+    //[Authorize(Policy = "Admin")]
+    public class AdminsOnlyModel : PageModel
+    {
+        private readonly ILogger<AdminsOnlyModel> _logger;
+
+        public AdminsOnlyModel(ILogger<AdminsOnlyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
--- ContosoPizza/Program.cs
+++ ContosoPizza/Program.cs
@@ -1,14 +1,14 @@
 using ContosoPizza.Data;
 using ContosoPizza.Services;
-
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity;
 using ContosoPizza.Areas.Identity.Data;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration
     .GetConnectionString("ContosoPizzaContextConnection") ?? throw new InvalidOperationException("Connection string 'ContosoPizzaContextConnection' not found.");
 builder.Services.AddDbContext<ContosoPizzaContext>(options => options.UseSqlite(connectionString));
+builder.Services.AddDefaultIdentity<ContosoPizzaUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ContosoPizzaContext>();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -16,7 +16,7 @@ builder.Services.AddScoped<PizzaService>();
 builder.Services.AddDbContext<PizzaContext>(options =>
     options.UseSqlite("Data Source=ContosoPizza.db"));
 
-builder.Services.AddDefaultIdentity<ContosoPizzaUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ContosoPizzaContext>();
+//builder.Services.AddDefaultIdentity<ContosoPizzaUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ContosoPizzaContext>();
 
 WebApplication app = builder.Build();
 
